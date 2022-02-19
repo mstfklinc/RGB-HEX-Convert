4d98c1a954cc63fb738a66218843c39f16a30e41
mstfklinc <mustafakilinc429@gmail.com>
2022-02-19 15:40:03 +0300

    Tasarım iyileştirmeleri yapıldı

--- RGB-HEX-Convert/ViewController.swift
+++ RGB-HEX-Convert/ViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var txtRed: UITextField!
+    @IBOutlet weak var txtGreen: UITextField!
+    @IBOutlet weak var txtBlue: UITextField!
+    @IBOutlet weak var lblResult: UILabel!
+    @IBOutlet weak var lblSelectedColor: UILabel!
+    @IBOutlet weak var btnConvert: UIButton!
+    var secilenIndex : Int = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func segmentChanged(_ sender: UISegmentedControl) {
+        
+        secilenIndex  = sender.selectedSegmentIndex
+        txtRed.text?.removeAll()
+        txtBlue.text?.removeAll()
+        txtGreen.text?.removeAll()
+        lblResult.text?.removeAll()
+        lblSelectedColor.backgroundColor = UIColor.white
+        
+        if secilenIndex == 0 {
+            
+            btnConvert.setTitle("RGB - HEX Convert", for: UIControl.State.normal)
+            
+        }else {
+            btnConvert.setTitle("HEX - RGB Convert", for: UIControl.State.normal)
+        }
+        
+    }
+    
+    
+    @IBAction func btnConvertClicked(_ sender: UIButton) {
+    }
+    
+    
 
 }
 
